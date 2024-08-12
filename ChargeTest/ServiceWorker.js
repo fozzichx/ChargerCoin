38002527dd06bd702db02c1cc4ee5dd03b46eadd
fozzichx <urevpavel17@gmail.com>
2024-08-13 02:26:48 +0500

    Add files via upload

---
+++ ChargeTest/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "fozzichx-ChargeCoin-1.0.2";
+const contentToCache = [
+    "Build/ChargeTest.loader.js",
+    "Build/ChargeTest.framework.js.br",
+    "Build/ChargeTest.data.br",
+    "Build/ChargeTest.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
